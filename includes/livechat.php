700b2d7750f38c708c72b98f299730bb16715e59
Erameh Michael <eramehmichael2000@gmail.com>
2024-07-21 04:53:32 +0100

    livechat

--- includes/livechat.php
+++ includes/livechat.php
@@ -7,4 +7,4 @@ $sqlUser = "SELECT * FROM settings WHERE id = '$settingsID'";
 $queryUser = mysqli_query($con, $sqlUser);
 $getde = mysqli_fetch_assoc($queryUser);
 
-echo $getde['livechat'];
+echo htmlspecialchars_decode($getde['livechat']);
