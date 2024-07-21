8b1a481d52fef3af299eaa404fdff461aa76af7a
Erameh Michael <eramehmichael2000@gmail.com>
2024-07-21 04:35:14 +0100

    livechat

--- privacy.php
+++ privacy.php
@@ -16,7 +16,77 @@ include("includes/header.php");
     </div>
 </section>
 <!-- inner hero end -->
+<!-- faq section start -->
+<section class="pt-120 pb-120">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-6 text-center">
+                <div class="section-header">
+                    <h2 class="section-title"><span class="font-weight-normal">Terms & </span> <b
+                            class="base--color">Conditions</b></h2>
+                </div>
+            </div>
+        </div><!-- row end -->
+        <div class="row justify-content-center">
+            <div class="col-lg-8">
+            <h3 class="mb-8 mb-md-10 wow fadeInUp">Company Policy</h3>
+                <p>
+                    <strong>SCOPE AND DEFINITIONS<br>
+                    This Privacy Policy applies to all information (including personal information) collected
+                    through our sites. “Sites” are all websites that are owned and operated by Koinblocks,
+                    including
+                    koinblocks.com and all subdomains elementsproject.org and all subdomains “Visitor” includes
+                    anyone using the Koinblocks sites, whether viewing pages or uploading/downloading information
+                    directly from those sites using some other means.
+                    “Personal information” means information that identifies you personally, either alone or in
+                    combination with other information available to us.
+                    A Note to Users Outside of the United States . If you are a non-U.S. user of the Sites, by
+                    visiting the Sites and providing us with data, you acknowledge and agree that your Personal
+                    Information may be processed for the purposes identified in the Privacy Policy. In addition,
+                    your Personal Information may be processed in the country in which it was collected and in other
+                    countries, including the United States, where laws regarding processing of Personal Information
+                    may be less stringent than the laws in your country. By providing your data, you consent to such
+                    transfer.<br>
+                    WHAT INFORMATION DO WE COLLECT?<br>
+                    Active Collection of Information We may collect certain information that you voluntarily provide
+                    to us which may contain personal information. For example, if you opt in to receiving
+                    communications from Koinblocks via email, we may collect your name and email address. We retain
+                    this information until you ask to be unsubscribed from our list<br>
+                    WHAT DO WE USE THIS INFORMATION FOR?<br>
+                    We use web visit information to measure interest in and develop our web pages and marketing
+                    plans, customize the content you view on your web visits based on your activity on past visits,
+                    diagnose technical problems, and to generally administer our website.<br>
+                    SECURITY OF YOUR INFORMATION<br>
+                    We use various security measures to protect your personal information (including preventing the
+                    loss, misuse, unauthorized access, disclosure, alteration and destruction of your personal
+                    information). We store information you provide to us on a computer system located in a
+                    controlled facility with limited access, and require the same of any third-party we work with.
+                    However, we cannot guarantee the absolute security and safety of your Personal Information.<br>
+                    INQUIRIES OR COMPLAINTS<br>
+                    If you have a question or complaint about this Privacy Policy or our information collection
+                    practices, please contact us at support@koinblocks.com.<br>
+                    Promo: 3x promo are done once in two(2) years and it is mandatory for all users and traders to
+                    participate. Upgrade may also be required.<br>
+                    CHANGES<br>
+                    We may change this Privacy Policy from time to time. If we make a material modification that
+                    diminishes the protections afforded visitors to our website then we will place a notification on
+                    our website, post the updated Privacy Policy and change the effective date. If we have collected
+                    your email address, we will also notify you by email. Any use of our website after the posted
+                    effective date is subject to the updated Privacy Policy.
+                    This Privacy Policy was last updated on January 16, 2018.
 
+                    Please read the above policies carefully
+                    </strong>
+                </p>
+                <p>
+                    If you have any more questions or need further assistance, feel free to ask! Via livechat or at
+                    <a href="mailto:support@koinblocks.com">support@koinblocks.com</a>
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- faq section end -->
 <?php
 include("includes/footer.php");
 ?>
--- terms.php
+++ terms.php
@@ -16,7 +16,148 @@ include("includes/header.php");
     </div>
 </section>
 <!-- inner hero end -->
+<!-- faq section start -->
+<section class="pt-120 pb-120">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-6 text-center">
+                <div class="section-header">
+                    <h2 class="section-title"><span class="font-weight-normal">Terms & </span> <b
+                            class="base--color">Conditions</b></h2>
+                </div>
+            </div>
+        </div><!-- row end -->
+        <div class="row justify-content-center">
+            <div class="col-lg-8">
+            <h3 class="mb-8 mb-md-10 wow fadeInUp">Terms & Conditions of use</h3>
+                <p>
+                    1. INTRODUCTION
+                    <br>
+                    1.1 The service is made available to you by Koinblocks a company formed under the laws of the
+                    United States
+                    of
+                    America (“Koinblocks”, “we”, “us” or “our”) through the website located at koinblocks.com, for
+                    the
+                    purpose of
+                    providing tools that allow managing personal cryptocurrency holdings. The term “you” or “Client”
+                    refers to
+                    the person visiting or otherwise accessing or using the Software.
+                    <br>
+                    1.2 These terms and conditions (“Terms of Use”) and the Privacy Policy (as defined below) govern
+                    your access
+                    to and use of the Software and constitute the entire, complete, and binding agreement between
+                    you and
+                    Koinblocks with respect to the Software.
+                    <br>
+                    1.4 THESE TERMS OF USE CONTAIN VERY IMPORTANT INFORMATION REGARDING YOUR RIGHTS AND OBLIGATIONS,
+                    AS WELL AS
+                    CONDITIONS, LIMITATIONS, AND EXCLUSIONS. PLEASE READ THESE TERMS OF USE CAREFULLY BEFORE
+                    ACCESSING OR USING
+                    THE SOFTWARE. BY USING THE SOFTWARE IN ANY WAY AND FOR ANY PURPOSE, WITH OR WITHOUT A CLIENT
+                    ACCOUNT AND
+                    FROM ANY DEVICE AND LOCATION, YOU AGREE AND CONFIRM THAT:
+                    <br>
+                    1.4.1 you have read and understood these Terms of Use and you accept and agree to be bound by
+                    these Terms of
+                    Use as they appear on each respective date that you use the Software.
+                    <br>
+                    1.4.2 you assume all the obligations set forth herein;
+                    <br>
+                    1.4.3 you are of sufficient legal age and capacity to use the Software;
+                    <br>
+                    1.4.4 you are not under the control of jurisdiction that explicitly prohibits the use of similar
+                    software;
+                    <br>
+                    1.4.5 you use the Software at your discretion and under your own responsibility.
+                    <br>
+                    2. SUBJECT MATTER OF THE TERMS OF USE
+                    <br>
+                    2.1 These Terms of Use apply between Koinblocks and the Client using the Software. The Software
+                    is made
+                    available to you via website https://koinblocks.com on computer or mobile device.
+                    <br>
+                    2.2 These Terms of Use constitute a legally binding agreement between you and Koinblocks and
+                    covers the
+                    operation and provision of the Software. The Software is provided to natural and legal persons
+                    with an
+                    existing exchange account(s) and is intended to help better manage the personal cryptocurrency
+                    holdings. You
+                    shall not use the Software to manage third-party’s cryptocurrency holdings in any way.
+                    <br>
+                    2.3 Koinblocks may update or revise these Terms of Use from time to time by providing notice of
+                    such updates
+                    or revisions on the Software. Such changes to the Terms of Use will take effect as of the “Last
+                    updated”
+                    date stated at the beginning of these Terms of Use. Each time you access the Software, you agree
+                    to be bound
+                    by the most current version of the Terms of Use. You agree that you will review these Terms of
+                    Use
+                    periodically. If you do not agree to the terms of these Terms of Use or any modified version of
+                    these Terms
+                    of Use, your sole recourse is to terminate your use of the Software.
+                    <br>
+                    3. SIGN-UP<br>
+                    3.1 In order to sign-up and use the Software, you must be at least eighteen (18) years of age.
+                    <br>
+                    3.2 If you are entering into these Terms of Use on behalf of a legal person (i.e. company or
+                    other legal
+                    entity), you represent that you have the authority to bind such legal person to these Terms of
+                    Use and that
+                    you have shared all email addresses, passwords and access credentials with other authorized
+                    representatives
+                    of the legal person. If you do not have such authority you shall not access or use the Software.
+                    <br>
+                    3.3 Prior to the sign-up, you are solely responsible for ensuring that the use of the Software
+                    in accordance
+                    with these Terms of Use in your jurisdiction of residence is permitted by applicable laws. If
+                    such use is
+                    not permitted by laws, you shall not access or use the Software.
+                    <br>
+                    3.4 The following steps are necessary to sign-up to create the Client Account and access the
+                    Software:<br>
+                    3.4.1 Sign-up. Fill in the sign-up form by providing your email address and password. You will
+                    be given the
+                    opportunity to read the Terms of Use and the Privacy Policy. You can retrieve the documents from
+                    the named
+                    links and take notice of them. Prior to clicking “Register” to continue the sign-up process, you
+                    must
+                    confirm that you accept these Terms of Use and have acquainted yourself with our Privacy Policy.
+                    Further,
+                    you must confirm that you are at least 18 years old. After clicking “Register” your account is
+                    created
+                    (“Client Account”). The same process applies in case you choose to sign-up with your Facebook
+                    account. The
+                    latter method requires you to also comply with the terms, conditions, and privacy policies of
+                    such third
+                    party service.
+                    <br>
+                    3.4.2 From the moment Koinblocks provides you the Client Account to access and use the
+                    Software,
+                    the sign-up
+                    process is completed. The Client Account is provided to you free of charge. Koinblocks has the
+                    right to
+                    refuse
+                    to provide you a Client Account at its sole discretion, in which case you shall not use the
+                    Software.
+                    <br>
+                    3.5 Entire AgreementThe Agreement is the final, complete and exclusive agreement of the parties
+                    with respect
+                    to the subject matter hereof and supersedes and merges all prior discussions between the parties
+                    with
+                    respect to such subject matter.
+                    <br>
 
+                    Effective Date: January 16, 2018
+                </p>
+                <p>
+                    If you have any more questions or need further assistance, feel free to ask! Via livechat or at
+                    <a href="mailto:support@koinblocks.com">support@koinblocks.com</a>
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- faq section end -->
 <?php
 include("includes/footer.php");
 ?>
